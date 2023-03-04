0497e92ff2a20e08d40ebd45c91976cc54aba5d5
CodeShaman33 <mar.donczyk@gmail.com>
2023-03-04 09:03:43 +0100

    generated API key and implemented it in a function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import {React, useEffect} from 'react';
+import './App.css'
+
+//20849a0e
+
+const API_URL = 'https://www.omdbapi.com?apikey=20849a0e';
+
+
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        
+        console.log(data);
+    };
+
+
+    useEffect(() => {
+        searchMovies('The Shawshank Redemption');
+        
+    }, []);
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1>Hello Wccorld</h1>
         </div>
     )
 }
